66b6b1ea21ab2581ab859d714049dcf30ebfe1cb
JPRSoares <139021424+JPRSoares@users.noreply.github.com>
2023-10-31 16:52:20 +0000

    Update Worker.java

--- src/io/codeforall/heapsdontlie/Worker.java
+++ src/io/codeforall/heapsdontlie/Worker.java
@@ -6,46 +6,52 @@ import java.net.Socket;
 public class Worker implements Runnable {
 
     private Socket clientSocket;
-    private  BufferedReader input;
+    private BufferedReader input;
     private BufferedWriter output;
+    private final String SERVER_ADDRESS = "localhost";
+    private final int SERVER_PORT = 8089;  // Presumi que o Worker se comunica com o Server na mesma porta. Se não for o caso, mude o valor.
 
     public Worker(Socket clientSocket) {
         this.clientSocket = clientSocket;
-
     }
 
-    public void reading (){
-
+    public String reading() {
+        String clientMessage = "";
         try {
             input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            input.readLine();
-
+            clientMessage = input.readLine();
         } catch (IOException e) {
             System.out.println(e);
         }
-
+        return clientMessage;
     }
 
-
-    public void writing (){
+    public void writing(String serverResponse) {
         try {
             output = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            String message = new String();
-            output.write(message);
+            output.write(serverResponse);
             output.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-
-
     @Override
     public void run() {
-        reading();
-        writing();
+        String clientMessage = reading();
 
-
-
+        // Send client's message to the Server and get the response
+        try (Socket serverSocket = new Socket(SERVER_ADDRESS, SERVER_PORT)) {
+            BufferedWriter serverOut = new BufferedWriter(new OutputStreamWriter(serverSocket.getOutputStream()));
+            BufferedReader serverIn = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+            
+            serverOut.write(clientMessage + "\n");  // Remember to add newline since we are using readLine
+            serverOut.flush();
+            
+            String serverResponse = serverIn.readLine();
+            writing(serverResponse);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
