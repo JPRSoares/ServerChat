d10d6990325b2248de8aea9584cd6be9ca2a8fc3
Jsoares <soareswebmarketing@gmail.com>
2023-11-02 16:18:28 +0000

    Server and Worker working.

--- src/io/codeforall/heapsdontlie/Server.java
+++ src/io/codeforall/heapsdontlie/Server.java
@@ -2,43 +2,46 @@ package io.codeforall.heapsdontlie;
 
 import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    int portNum = 8089;
+    private int portNum = 8089;
     private ServerSocket bindSocket = null;
-    private CopyOnWriteArrayList list;
+    private CopyOnWriteArrayList<Worker> workerList;
+
+    public Server() {
+        this.workerList = new CopyOnWriteArrayList<>();
+    }
 
     public static void main(String[] args) {
         Server server = new Server();
         server.listen(server.portNum);
-        CopyOnWriteArrayList list = new CopyOnWriteArrayList<>();
     }
 
     private void listen(int portNum) {
         try {
             bindSocket = new ServerSocket(portNum);
             System.out.println("Server bind");
-            dispatch(bindSocket);
+            dispatch();
         } catch (IOException e) {
             System.out.println(e);
         }
     }
 
-    private void dispatch(ServerSocket bindSocket) {
+    private void dispatch() {
         ExecutorService cachedPool = Executors.newCachedThreadPool();
         while (true) {
             try {
-                Worker worker = new Worker(bindSocket.accept());
+                Socket clientSocket = bindSocket.accept();
+                Worker worker = new Worker(clientSocket, workerList);
+                workerList.add(worker);
                 cachedPool.submit(worker);
             } catch (IOException e) {
                 System.out.println(e);
             }
-
         }
-
     }
-
 }
--- src/io/codeforall/heapsdontlie/Worker.java
+++ src/io/codeforall/heapsdontlie/Worker.java
@@ -2,48 +2,66 @@ package io.codeforall.heapsdontlie;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
 
 public class Worker implements Runnable {
 
     private final Socket clientSocket;
+    private final List<Worker> workerList;
 
-    public Worker(Socket clientSocket) {
+    public Worker(Socket clientSocket, List<Worker> workerList) {
         this.clientSocket = clientSocket;
+        this.workerList = workerList;
     }
 
-    public String reading() {
-        String clientMessage = "";
-        try {
-            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            clientMessage = input.readLine();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        return clientMessage;
+    private String reading() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        return input.readLine();
     }
 
-    public void writing(String serverResponse) {
-        try {
-            BufferedWriter output = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            output.write(serverResponse);
-            output.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void writing(String message) throws IOException {
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        output.write(message);
+        output.flush();
+    }
+
+    public void broadcast(String message) {
+        for (Worker worker : workerList) {
+            try {
+                if (!worker.clientSocket.isClosed()) {
+                    worker.writing(message + "\n");
+                }
+            } catch (IOException e) {
+                System.out.println("Error broadcasting to worker: " + e.getMessage());
+            }
         }
     }
 
     @Override
     public void run() {
-        String clientMessage = reading();
-        while (true) {
-            {
-                int SERVER_PORT = 8089;
-                String SERVER_ADDRESS = "localhost";
-                writing(clientMessage + "\n");
-
+        try {
+            while (!clientSocket.isClosed()) {
+                String clientMessage = reading();
+                if (clientMessage == null) {
+                    break;
+                }
+                broadcast(clientMessage);
             }
+        } catch (IOException e) {
+            System.out.println("Worker run: " + e.getMessage());
+        } finally {
+            cleanup();
         }
-
     }
 
+    private void cleanup() {
+        workerList.remove(this);
+        try {
+            if (!clientSocket.isClosed()) {
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Error closing worker socket: " + e.getMessage());
+        }
+    }
 }
